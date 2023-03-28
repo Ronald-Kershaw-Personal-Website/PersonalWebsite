9c8c93b1a745b46d666c9e06fb2ffb6ef05d09ea
R-Kershaw <rkershaw90@gmail.com>
2023-03-28 04:27:10 -0400

    added a function that creates a popup window when a certain event occurs

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//external javascript file. create a hello world tier javascript function, or possibly retrieve the date and display it somewhere
+//external javascript file. create a hello world tier javascript function
+
+//display an alert message when an event occurs (like clicking on a link)
+function popUpFunction()
+{
+    const message = "Hire me :}";
+    alert(message);
+    console.log("PopUpFunction called successfully");
+}
