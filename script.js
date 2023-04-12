5bbb3080247a37786dab6fce1639e2d254d5572f
R-Kershaw <rkershaw90@gmail.com>
2023-04-12 01:03:05 -0400

    Added new form send and recieve pages and new javascript code

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //external javascript file. create a hello world tier javascript function
 
 //display an alert message when an event occurs (like clicking on a link)
-function popUpFunction()
-{
+function popUpFunction() {
     const message = "Hire me :}";
     alert(message);
     console.log("PopUpFunction called successfully");
 }
+
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+const scriptURL = 'https://httpbin.org/post';
+
+form.addEventListener('submit', e => {
+    submitButton.disabled = true
+    e.preventDefault()
+    let requestBody = new FormData(form)
+    fetch(scriptURL, { method: 'POST', body: requestBody })
+        .then(response => {
+            alert('Success!', response)
+            submitButton.disabled = false
+        })
+        .catch(error => {
+            alert('Error!', error.message)
+            submitButton.disabled = false
+
+        }
+        )
+});
